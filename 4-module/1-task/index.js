8a99f1d79c301e75e505e6f5fb63d732bf5011c3
Eliseev Andrey <endorph3ne@gmail.com>
2022-05-19 02:37:56 +0300

    Added 4-module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+
+  for (let current of friends) {
+    ul.insertAdjacentHTML('beforeend', `<li>${current.firstName} ${current.lastName}</li>`);
+  }
+
+  return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,19 @@
 function highlight(table) {
-  // ваш код...
+  for (let currentRow of table.rows) {
+    for (let currentCell of currentRow.cells) {
+      if (currentCell.hasAttribute('data-available')) {
+        let className = currentCell.getAttribute('data-available') === 'true' ? 'available' : 'unavailable';
+        currentRow.classList.add(className);
+      } else {
+        currentRow.setAttribute('hidden', 'true');
+      }
+
+      let classNameGender = currentCell.textContent === 'm' ? 'male' : 'female';
+      currentRow.classList.add(classNameGender);
+
+      if (+currentCell.textContent < 18) {
+        currentRow.style.textDecoration = 'line-through';
+      }
+    }
+  }
 }
