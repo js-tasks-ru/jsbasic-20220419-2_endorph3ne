a9a82a265460b240031a1f1b4ff7ce45108bf82b
Eliseev Andrey <endorph3ne@gmail.com>
2022-05-24 16:52:43 +0300

    Added 6-module/1 task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,44 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.createTable();
+    this.deleteRows();
+  }
+
+  createTable() {
+    let table = document.createElement("table");
+    let thead = document.createElement('thead');
+    let tbody = document.createElement('tbody');
+
+    table.append(thead);
+    thead.innerHTML = '<tr> <th>Имя</th> <th>Возраст</th> <th>Зарплата</th> <th>Город</th> <th></th> </tr>';
+
+    table.append(tbody);
+    for (let current of this.rows) {
+      let rowTable = document.createElement('tr');
+
+      let button = ''
+      rowTable.insertAdjacentHTML('beforeend',
+        `<td>${current.name}</td>
+        <td>${current.age}</td>
+        <td>${current.salary}</td>
+        <td>${current.city}</td>
+        <td><button>X</button></td>`);
+      tbody.append(rowTable);
+    }
+
+    return table;
+  }
+
+  deleteRows() {
+    let buttons = this.elem.querySelectorAll('button');
+
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener('click', (e) => {
+        let tableRow = e.target.closest('tr');
+        tableRow.parentNode.removeChild(tableRow);
+      })
+    }
   }
 }
