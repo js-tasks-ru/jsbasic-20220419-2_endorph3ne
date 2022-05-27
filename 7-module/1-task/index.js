107db03758acdbd5714e2e5d7e4b570d0d16899c
Eliseev Andrey <endorph3ne@gmail.com>
2022-05-27 17:32:26 +0300

    Added 7-module/1,2 tasks

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -3,5 +3,117 @@ import createElement from '../../assets/lib/create-element.js';
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+
+    this.elem = this.createRibbon();
+    this.createScrollEvents();
+    this.createRibbonSelectCustomItem();
+  }
+
+  createRibbon() {
+    let divRibbon = document.createElement('div');
+    divRibbon.className = 'ribbon';
+
+    let ribbonArrowLeft = document.createElement('button');
+    ribbonArrowLeft.classList.add('ribbon__arrow');
+    ribbonArrowLeft.classList.add('ribbon__arrow_left');
+
+    let imgArrowLeft = document.createElement('img');
+    imgArrowLeft.src = '/assets/images/icons/angle-icon.svg';
+    imgArrowLeft.setAttribute('alt', 'icon');
+
+    ribbonArrowLeft.append(imgArrowLeft);
+
+    let ribbonArrowRight = document.createElement('button');
+    ribbonArrowRight.classList.add('ribbon__arrow');
+    ribbonArrowRight.classList.add('ribbon__arrow_right');
+    ribbonArrowRight.classList.add('ribbon__arrow_visible');
+
+    let imgArrowRight = document.createElement('img');
+    imgArrowRight.src = '/assets/images/icons/angle-icon.svg';
+    imgArrowRight.setAttribute('alt', 'icon');
+
+    ribbonArrowRight.append(imgArrowRight);
+
+
+    divRibbon.append(ribbonArrowLeft);
+    divRibbon.append(this.createNavRibbonInner());
+    divRibbon.append(ribbonArrowRight);
+
+    return divRibbon;
+  }
+  createNavRibbonInner() {
+    let navRibbonInner = document.createElement('nav');
+    navRibbonInner.className = 'ribbon__inner';
+
+    for (let i = 0; i < this.categories.length; i++) {
+      let a = document.createElement('a');
+      a.classList.add('ribbon__item');
+      a.href = '#';
+      a.innerHTML = this.categories[i].name;
+
+      if (i === 0) {
+        a.classList.add('ribbon__item_active');
+        a.dataset.id = '';
+      }
+      else {
+        a.dataset.id = this.categories[i].id;
+      }
+
+      navRibbonInner.append(a);
+    }
+
+    return navRibbonInner;
+  }
+
+  createScrollEvents() {
+    let ribbonArrowLeft = this.elem.querySelector('.ribbon__arrow_left');
+    let ribbonInner = this.elem.querySelector('.ribbon__inner');
+    let ribbonArrowRight = this.elem.querySelector('.ribbon__arrow_right');
+
+    ribbonArrowLeft.addEventListener('click', () => {
+      ribbonInner.scrollBy(-350, 0);
+    });
+
+    ribbonArrowRight.addEventListener('click', () => {
+      ribbonInner.scrollBy(350, 0);
+    });
+
+    ribbonInner.addEventListener('scroll', () => {
+      let scrollLeft = ribbonInner.scrollLeft;
+      if (scrollLeft === 0) {
+        ribbonArrowLeft.classList.remove('ribbon__arrow_visible');
+      }
+      else {
+        ribbonArrowLeft.classList.add('ribbon__arrow_visible');
+      }
+
+      let scrollWidth = ribbonInner.scrollWidth;
+      let clientWidth = ribbonInner.clientWidth;
+      let scrollRight = scrollWidth - scrollLeft - clientWidth;
+      if (scrollRight === 0) {
+        ribbonArrowRight.classList.remove('ribbon__arrow_visible');
+      }
+      else {
+        ribbonArrowRight.classList.add('ribbon__arrow_visible');
+      }
+    });
+  }
+  createRibbonSelectCustomItem() {
+    let anchors = this.elem.querySelectorAll('.ribbon__item');
+
+    for (let i = 0; i < this.categories.length; i++) {
+      anchors[i].addEventListener('click', (event) => {
+        let activeItem = this.elem.querySelector('.ribbon__item_active');
+        activeItem.classList.remove('ribbon__item_active');
+        anchors[i].classList.add('ribbon__item_active');
+
+        let ribbonSelectCustomEvent = new CustomEvent('ribbon-select', {
+          detail: this.categories[i].id,
+          bubbles: true,
+        });
+
+        anchors[i].dispatchEvent(ribbonSelectCustomEvent);
+      });
+    }
   }
 }
--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,93 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this.elem = this.createModal();
+    this.createEvents();
+  }
+
+  createModal() {
+    let divModal = document.createElement('div');
+    divModal.className = 'modal';
+
+    let divModalOverlay = document.createElement('div');
+    divModalOverlay.className = 'modal__overlay';
+
+    divModal.append(divModalOverlay);
+    divModal.append(this.createModalInner());
+
+    return divModal;
+  }
+  createModalInner() {
+    let divModalInner = document.createElement('div');
+    divModalInner.className = 'modal__inner';
+
+    let divModalBody = document.createElement('div');
+    divModalBody.className = 'modal__body';
+
+    divModalInner.append(this.createModalHeader());
+    divModalInner.append(divModalBody);
+
+    return divModalInner;
+  }
+  createModalHeader() {
+    let divModalHeader = document.createElement('div');
+    divModalHeader.className = 'modal__header';
+
+    let buttonModalClose = document.createElement('button');
+    buttonModalClose.className = 'modal__close';
+
+    let img = document.createElement('img');
+    img.src = '/assets/images/icons/cross-icon.svg';
+    img.setAttribute('alt', 'close-icon');
+
+    buttonModalClose.append(img);
+
+    let h3ModalTitle = document.createElement('h3');
+    h3ModalTitle.className = 'modal__title';
+
+    divModalHeader.append(buttonModalClose);
+    divModalHeader.append(h3ModalTitle);
+
+    return divModalHeader;
+  }
+
+  open() {
+    let body = document.querySelector('body');
+    body.className = 'is-modal-open';
+
+    body.append(this.elem);
+  }
+  setTitle(title) {
+    let h3ModalTitle = this.elem.querySelector('.modal__title');
+    h3ModalTitle.innerHTML = title;
+  }
+  setBody(node) {
+    let divModalBody = this.elem.querySelector('.modal__body');
+    divModalBody.append(node);
+  }
+  close() {
+    let body = document.querySelector('body');
+    body.className = '';
+    body.innerHTML = '';
+
+    document.onkeydown = null;
+  }
+
+  createEvents() {
+    let button = this.elem.querySelector('.modal__close');
+
+    button.addEventListener('click', () => {
+      this.close();
+    });
+
+    document.onkeydown = function (event) {
+      if (event.code === 'Escape') {
+        let body = document.querySelector('body');
+        body.className = '';
+        body.innerHTML = '';
+
+        document.onkeydown = null;
+      }
+    };
   }
 }
